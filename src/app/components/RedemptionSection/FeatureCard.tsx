7fc8a3d029692d5ab00191630a16021e7433e524
Lobanov Daniil <lobanovdaniil@MacBook-Pro-Lobanov.local>
2024-11-08 05:36:07 +0300

    animation for redemptions btns

--- src/app/components/RedemptionSection/FeatureCard.tsx
+++ src/app/components/RedemptionSection/FeatureCard.tsx
@@ -1,7 +1,6 @@
-import React, { ReactNode, useMemo } from 'react';
-import styled from '@emotion/styled';
+import React, { useMemo } from 'react';
 import { useTheme } from "next-themes";
-import Image from "next/image";
+import { ActionButton, BtnsContentWrapper, CardContent, CardTitle, FeatureCardContainer, FeatureImage, IconArrowRightStyled } from './FeatureCard.styles';
 
 export interface FeatureCardProps {
     title: string;
@@ -16,73 +15,11 @@ export interface FeatureCardProps {
     buttonColorDark: string;
 }
 
-const FeatureCardContainer = styled.div<{ isDark: boolean, backgroundColor: string, backgroundColorDark: string, borderColor: string, borderColorDark: string }>`
-    flex: 1;
-    min-width: 240px;
-    border-radius: 16px;
-    padding: 16px;
-    display: flex;
-    gap: 16px;
-    overflow: hidden;
-    background-color: ${({ isDark, backgroundColor, backgroundColorDark }) => isDark ? backgroundColorDark : backgroundColor};
-    border: 3px solid ${({ isDark, borderColor, borderColorDark }) => isDark ? borderColorDark : borderColor};
-    :hover {
-        background: ${({ isDark, borderColor, borderColorDark }) => isDark ? borderColorDark : borderColor};
-    }
-    transition: background 0.3s ease;
-    
-    @media screen and (max-width: 1280px) {
-        height: 340px;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: flex-start;
-    }
-    @media screen and (max-width: 1024px) {
-        height: 460px;
-        width: 416px;
-    }
-`;
-
-const CardContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    flex: 1;
-`;
-
-const ActionButton = styled.button<{ isDark: boolean, buttonColor: string, buttonColorDark: string }>`
-    border-radius: 100px;
-    background-color: ${({ isDark }) => isDark ? 'black' : 'white'};
-    display: flex;
-    align-items: center;
-    gap: 8px;
-    justify-content: center;
-    padding: 8px 18px;
-    font: 400 24px var(--Font-Body, Syne);
-    border: none;
-    cursor: pointer;
-    width: fit-content;
-    color: ${({ isDark, buttonColor, buttonColorDark }) => isDark ? buttonColorDark : buttonColor};
-`;
-
-const CardTitle = styled.h2`
-    font: 700 38px var(--Font-Titles, Syne);
-    font-size: 38px;
-
-    @media screen and (max-width: 1024px) {
-        height: 32px;
-    }
-`;
-
-const FeatureImage = styled.img`
-    width: 260px;
-    object-fit: contain;
-`;
 
 export const FeatureCard: React.FC<FeatureCardProps> = ({
     title,
     buttonText,
-    icon,
+    icon: Icon,
     imageSrc,
     backgroundColor,
     backgroundColorDark,
@@ -108,8 +45,13 @@ export const FeatureCard: React.FC<FeatureCardProps> = ({
                     buttonColor={buttonColor}
                     buttonColorDark={buttonColorDark}
                 >
-                    {React.createElement(icon)}
-                    <span>{buttonText}</span>
+                    <BtnsContentWrapper id='btns-content-wrapper'>
+                        <BtnsContentWrapper>
+                            <Icon/>
+                            <span>{buttonText}</span>
+                            <IconArrowRightStyled />
+                        </BtnsContentWrapper>
+                    </BtnsContentWrapper>
                 </ActionButton>
                 <CardTitle>{title}</CardTitle>
             </CardContent>
